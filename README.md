acaeb55deb7bfe729730c145a99effc0f3295c6a
xieyucheng <xieyucheng@dyg.cn>
2020-05-05 18:37:49 +0800

    添加Yii2框架类获取完整模块ID方法

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ $ composer require xihrni/php-tools
 ```
 
 ## Demo
+### Other
 ```php
 <?php
 
@@ -16,4 +17,18 @@ use xihrni\tools\Other;
 
 // $data = ...;
 Other::dump($data);
+```
+
+### Yii2
+```php
+<?php
+
+use xihrni\tools\Yii2;
+
+// ...
+
+public function beforeAction($action)
+{
+    $moduleId = Yii2::getFullModuleId($action->controller->module, $ids = []);
+}
 ```
---
+++ src/Yii2.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace xihrni\tools;
+
+use Yii;
+
+/**
+ * Yii2框架类
+ *
+ * Class Yii2
+ * @package xihrni\tools
+ */
+class Yii2
+{
+    /**
+     * 获取完整模块ID
+     *
+     * @param  object $module 模块对象
+     * @param  array  $ids    临时存放当前已获取的模块ID
+     * @return array
+     */
+    public function getFullModuleId($module, array &$ids)
+    {
+        if (isset($module->id)) {
+            $ids[] = $module->id;
+
+            if ($module->module) {
+                // 过滤框架ID
+                if ($module->module->id != Yii::$app->id) {
+                    $this->getFullModuleId($module->module, $ids);
+                }
+            }
+        }
+
+        return $ids;
+    }
+}
